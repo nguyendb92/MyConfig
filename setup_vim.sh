5e635bda2672e79194cd3f408c9e4a1c6f052778
nguyendb92 <nguyendb92@gmail.com>
2022-11-27 23:56:19 +0700

    fix colors

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.vim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -fLo ~/.vim/colors/monokai.vim --create-dirs https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 cp vimrc ~/.vimrc
 
 
